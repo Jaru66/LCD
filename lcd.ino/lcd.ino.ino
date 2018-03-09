e8384366e74fa40d086ec2d8cfb452825ce752e8
Jaru66 <34154018+Jaru66@users.noreply.github.com>
2018-03-09 18:52:06 +0100

    głośnik
    Dodałem funkcje głośnika, po naciśnięciu select głośnik piszczy
    Glośnik jest na pinie 3

---
+++ lcd.ino/lcd.ino.ino
@@ -0,0 +1,55 @@
+#include <LiquidCrystal.h>
+
+LiquidCrystal lcd(8,9,4,5,6,7);
+
+#define Glosnik 3
+#define btnRIGHT  0
+#define btnUP     1
+#define btnDOWN   2
+#define btnLEFT   3
+#define btnSELECT 4
+#define btnNONE   5
+
+int lcd_key     =0;
+int adc_key_in  =0;
+
+int read_LCD_buttons(){               // wczytanie przycisku a
+    adc_key_in = analogRead(0);       // wczytaj wartość z sensora
+
+    // my buttons when read are centered at these valies: 0, 144, 329, 504, 741
+    // we add approx 50 to those values and check to see if we are close
+    // We make this the 1st option for speed reasons since it will be the most likely result
+
+    if (adc_key_in > 1000) return btnNONE; 
+
+    // For V1.1 us this threshold
+    if (adc_key_in < 50)   return btnRIGHT;  
+    if (adc_key_in < 250)  return btnUP; 
+    if (adc_key_in < 450)  return btnDOWN; 
+    if (adc_key_in < 650)  return btnLEFT;
+    if (adc_key_in < 850)  return btnSELECT;  
+
+   // For V1.0 comment the other threshold and use the one below:
+
+    return btnNONE;   // when all others fail, return this.
+}
+
+int beep()
+{
+  lcd.begin(16,2);
+  lcd.setCursor(0,0);
+  lcd.print("PRESS SELECT");
+  if(read_LCD_buttons()==btnSELECT)
+  {
+    tone(Glosnik,4000,200);
+    }  
+  }
+void setup() {
+  
+
+}
+
+void loop() {
+  beep();
+
+}
